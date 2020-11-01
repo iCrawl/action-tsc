cf1210f5dd24ae1245b91d5d3e57580f8827c679
Piotr Banasik <piotr.banasik@gmail.com>
2020-11-01 06:13:41 -0800

    Make things a bit more generic by allowing to override the `tsc` executable (#216)

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { join } from 'path';
 async function run() {
 	const project = getInput('project');
 	const build = getInput('build');
+	const executable = getInput('executable');
 	console.log(`##[add-matcher]${join(__dirname, '..', '.github', 'tsc.json')}`);
 	const args = [
-		`${join(process.cwd(), 'node_modules/typescript/bin/tsc')}`,
+		`${join(process.cwd(), 'node_modules/.bin', executable)}`,
 		'--noEmit',
 		'--noErrorTruncation',
 		'--pretty',
